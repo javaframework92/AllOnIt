4b44869546e9d2ae006eaaac2f9a5c58261caeb7
Ajeet <Ajeet@LAPTOP-93MNBBNC>
2020-09-27 14:15:27 +0530

    Add CaptchUtil class

---
+++ src/main/java/com/ajt/util/CaptchaUtil.java
@@ -0,0 +1,42 @@
+package com.ajt.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+import javax.imageio.ImageIO;
+
+import cn.apiclub.captcha.Captcha;
+
+public class CaptchaUtil {
+
+	private CaptchaUtil() {
+
+	}
+
+	public static Captcha createCaptcha(int width, int height) {
+		return new Captcha.Builder(width, height).addBackground().addText().addNoise().build();
+	}
+
+	public static ByteArrayOutputStream createImage(Captcha captcha, String formatName, String file)
+			throws IOException {
+		ByteArrayOutputStream baos = null;
+		if (Objects.nonNull(captcha)) {
+			baos = new ByteArrayOutputStream();
+			ImageIO.write(captcha.getImage(), formatName, baos);
+		}
+		return baos;
+	}
+
+	public static void writeFile(ByteArrayOutputStream baos, String file) throws IOException {
+		if (Objects.nonNull(baos)) {
+			FileOutputStream fos = new FileOutputStream(new File(file));
+			fos.write(baos.toByteArray());
+			fos.flush();
+			fos.close();
+		}
+	}
+
+}
